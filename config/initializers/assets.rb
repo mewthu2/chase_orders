7454015dc6be00f309741182a2a596dc1b5d888a
danielkbr <danielkbr89@gmail.com>
2023-10-16 00:34:36 -0300

    trying to fix image issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,16 +4,24 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
-# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Emoji.images_path if defined?(Emoji) # If you're using Emoji assets
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('vendor/assets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[*.js *.scss]
-Rails.application.config.assets.precompile += %w[admin/views/*.css]
-Rails.application.config.assets.precompile += %w[admin/views/*.js]
 
+# Specify which specific assets to precompile:
+Rails.application.config.assets.precompile += %w(
+  admin.js
+  admin.css
+  *.js
+  *.scss
+  admin/views/*.css
+  admin/views/*.js
+)
+
+# If you have additional asset types, add them here:
+# Rails.application.config.assets.precompile += %w( admin/images/*.png )
