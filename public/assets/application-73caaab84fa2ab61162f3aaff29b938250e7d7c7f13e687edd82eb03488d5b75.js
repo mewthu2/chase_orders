fc18bcee75b63811fa6b8218b13c9f3e445526f3
danielkbr <danielkbr89@gmail.com>
2023-10-16 00:21:28 -0300

    change assets

--- public/assets/application-73caaab84fa2ab61162f3aaff29b938250e7d7c7f13e687edd82eb03488d5b75.js
+++
@@ -1,6 +0,0 @@
-const app = createApp()
-
-document.addEventListener("turbo:load", () => {
-  app.load()
-  Turbo.connectStreamSource()
-});
--- public/assets/application-b7613cde4bd27ddede11a090863415c47a48ebeb2da3fb7beab5d3e1bbac3b79.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/js/bootstrap.bundle"
-
-const app = createApp()
-
-document.addEventListener("turbo:load", () => {
-  app.load()
-  Turbo.connectStreamSource()
-});
