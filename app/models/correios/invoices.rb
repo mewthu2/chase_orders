05a8567c5333bffb97d0fcff91f701668fdb8527
danielkbr <danielkbr89@gmail.com>
2023-11-04 23:02:15 -0300

    test

--- app/models/correios/invoices.rb
+++ app/models/correios/invoices.rb
@@ -17,17 +17,18 @@ class Correios::Invoices
       attempt.update(error: e, status: :error)
     end
 
+    begin
+      tracking = Correios::Orders.get_tracking(attempt.order_correios_id)
+    rescue StandardError => e
+      attempt.update(error: e, message: 'Erro ao solicitar o rastreio', status: :error)
+    end
+
     if response&.body.present?
       case response.code
       when 200
-        attempt.update(status: :success, xml_sended: true, status_code: response.code) if response.body == attempt.xml_nota
+        attempt.update(status: :success, xml_sended: true, status_code: response.code) if response.body == attempt.xml_nota && response.body.include?('faturado') && tracking.present? && tracking['rastreio'].present? && tracking['rastreio'][0].present?
       when 400
-        begin
-          tracking = Correios::Orders.get_tracking(attempt.order_correios_id)
-        rescue StandardError => e
-          attempt.update(error: e, message: 'Erro ao solicitar o rastreio', status: :error)
-        end
-        if response.body.include?('faturado') && racking.present? && tracking['rastreio'].present? && tracking['rastreio'][0].present?
+        if response.body.include?('faturado') && tracking.present? && tracking['rastreio'].present? && tracking['rastreio'][0].present?
           attempt.update(status: :success, status_code: response.code, message: response.body, xml_sended: true, tracking: tracking['rastreio'][0]['codigoObjeto'])
         else
           attempt.update(status: :error, status_code: response.code, message: response.body)
