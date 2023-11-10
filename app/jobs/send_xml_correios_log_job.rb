5ac198a19374b9f2c51264e4e64d1a691a86dc39
danielkbr <danielkbr89@gmail.com>
2023-11-10 09:54:12 -0300

    change xml

--- app/jobs/send_xml_correios_log_job.rb
+++ app/jobs/send_xml_correios_log_job.rb
@@ -31,8 +31,9 @@ class SendXmlCorreiosLogJob < ActiveJob::Base
     doc.traverse do |node|
       node.content = att.order_correios_id.to_s if node.element? && node.name == 'xPed'
     end
+    xml_nfe_content = doc.at('xml_nfe').inner_html
 
-    attempt.update(xml_nota: doc.to_xml.gsub("\n", ''),
+    attempt.update(xml_nota: xml_nfe_content.to_xml.gsub("\n", ''),
                    order_correios_id: att.order_correios_id,
                    id_nota_fiscal: att.id_nota_fiscal,
                    tiny_order_id: att.tiny_order_id)
