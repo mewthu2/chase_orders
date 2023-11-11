6950e7441a6ac3fd9be3936b0ef1c8ab4a676682
danielkbr <danielkbr89@gmail.com>
2023-11-11 06:09:06 -0300

    slow send xml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_10_202855) do
-  create_table "attempts", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "attempts", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "kinds"
     t.bigint "status"
     t.text "requisition"
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_202855) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.string "phone"
     t.string "email", default: "", null: false
