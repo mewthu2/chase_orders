401b35f67fd661f31821d34ad2b794313d68bf96
danielkbr <danielkbr89@gmail.com>
2024-11-04 01:30:19 -0300

    update spreadsheets

---
+++ app/jobs/make_csv_data_job.rb
@@ -0,0 +1,10 @@
+class MakeCsvDataJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    MakeSpreadsheetJob.perform_now('lagoa_seca', 'product')
+    MakeSpreadsheetJob.perform_now('lagoa_seca', 'order')
+    MakeSpreadsheetJob.perform_now('bh_shopping', 'product')
+    MakeSpreadsheetJob.perform_now('bh_shopping', 'order')
+  end
+end
