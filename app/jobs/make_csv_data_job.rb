37484fa6a5771529d9d92bf3cd80310e42ad7e91
danielkbr <danielkbr89@gmail.com>
2024-12-16 09:04:06 -0300

    update stock

--- app/jobs/make_csv_data_job.rb
+++ app/jobs/make_csv_data_job.rb
@@ -2,14 +2,26 @@ class MakeCsvDataJob < ApplicationJob
   queue_as :default
 
   def perform
-    SyncProductsSituationJob.perform_now
-    Tiny::Products.assert_stock
-    SyncOrdersSituationJob.perform_now
-    MakeSpreadsheetJob.perform_now('lagoa_seca', 'product')
-    MakeSpreadsheetJob.perform_now('lagoa_seca', 'order')
-    MakeSpreadsheetJob.perform_now('bh_shopping', 'product')
-    MakeSpreadsheetJob.perform_now('bh_shopping', 'order')
-    MakeSpreadsheetJob.perform_now('rj', 'product')
-    MakeSpreadsheetJob.perform_now('rj', 'order')
+    log_job('SyncProductsSituationJob') { SyncProductsSituationJob.perform_now }
+    log_job('Tiny::Products.assert_stock') { Tiny::Products.assert_stock }
+    log_job('SyncOrdersSituationJob') { SyncOrdersSituationJob.perform_now }
+    log_job('MakeSpreadsheetJob - lagoa_seca, product') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'product') }
+    log_job('MakeSpreadsheetJob - lagoa_seca, order') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'order') }
+    log_job('MakeSpreadsheetJob - bh_shopping, product') { MakeSpreadsheetJob.perform_now('bh_shopping', 'product') }
+    log_job('MakeSpreadsheetJob - bh_shopping, order') { MakeSpreadsheetJob.perform_now('bh_shopping', 'order') }
+    log_job('MakeSpreadsheetJob - rj, product') { MakeSpreadsheetJob.perform_now('rj', 'product') }
+    log_job('MakeSpreadsheetJob - rj, order') { MakeSpreadsheetJob.perform_now('rj', 'order') }
+  end
+
+  private
+
+  def log_job(job_name)
+    Rails.logger.info "=== Início do #{job_name} ==="
+    start_time = Time.now
+
+    yield
+
+    duration = Time.now - start_time
+    Rails.logger.info "=== Fim do #{job_name} (Tempo de execução: #{duration.round(2)} segundos) ==="
   end
 end
