44808b3cad4776b8af5030292b5c1cd9cae4777a
danielkbr <danielkbr89@gmail.com>
2024-12-20 10:22:22 -0300

    update job info

--- app/jobs/make_csv_data_job.rb
+++ app/jobs/make_csv_data_job.rb
@@ -1,16 +1,20 @@
 class MakeCsvDataJob < ApplicationJob
   queue_as :default
 
-  def perform
-    log_job('SyncProductsSituationJob') { SyncProductsSituationJob.perform_now }
-    log_job('Tiny::Products.assert_stock') { Tiny::Products.assert_stock }
-    log_job('SyncOrdersSituationJob') { SyncOrdersSituationJob.perform_now }
-    log_job('MakeSpreadsheetJob - lagoa_seca, product') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'product') }
-    log_job('MakeSpreadsheetJob - lagoa_seca, order') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'order') }
-    log_job('MakeSpreadsheetJob - bh_shopping, product') { MakeSpreadsheetJob.perform_now('bh_shopping', 'product') }
-    log_job('MakeSpreadsheetJob - bh_shopping, order') { MakeSpreadsheetJob.perform_now('bh_shopping', 'order') }
-    log_job('MakeSpreadsheetJob - rj, product') { MakeSpreadsheetJob.perform_now('rj', 'product') }
-    log_job('MakeSpreadsheetJob - rj, order') { MakeSpreadsheetJob.perform_now('rj', 'order') }
+  def perform(kind)
+    case kind
+    when 'update_info'
+      log_job('SyncProductsSituationJob') { SyncProductsSituationJob.perform_now }
+      log_job('Tiny::Products.assert_stock') { Tiny::Products.assert_stock }
+      log_job('SyncOrdersSituationJob') { SyncOrdersSituationJob.perform_now }
+    when 'generate'
+      log_job('MakeSpreadsheetJob - lagoa_seca, product') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'product') }
+      log_job('MakeSpreadsheetJob - lagoa_seca, order') { MakeSpreadsheetJob.perform_now('lagoa_seca', 'order') }
+      log_job('MakeSpreadsheetJob - bh_shopping, product') { MakeSpreadsheetJob.perform_now('bh_shopping', 'product') }
+      log_job('MakeSpreadsheetJob - bh_shopping, order') { MakeSpreadsheetJob.perform_now('bh_shopping', 'order') }
+      log_job('MakeSpreadsheetJob - rj, product') { MakeSpreadsheetJob.perform_now('rj', 'product') }
+      log_job('MakeSpreadsheetJob - rj, order') { MakeSpreadsheetJob.perform_now('rj', 'order') }
+    end
   end
 
   private
