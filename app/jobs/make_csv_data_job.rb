9c0d1b6ac2fe7157208ce5b5b61e65f5a1b5fac4
danielkbr <danielkbr89@gmail.com>
2024-12-22 23:24:13 -0300

    assert jobs

--- app/jobs/make_csv_data_job.rb
+++ app/jobs/make_csv_data_job.rb
@@ -2,9 +2,9 @@ class MakeCsvDataJob < ApplicationJob
   queue_as :default
 
   def perform
-    log_job('SyncProductsSituationJob') { SyncProductsSituationJob.perform_now }
-    log_job('Tiny::Products.assert_stock') { Tiny::Products.assert_stock }
-    log_job('SyncOrdersSituationJob') { SyncOrdersSituationJob.perform_now }
+    log_job('SyncProductsSituationJob') { log_motor_activity('SyncProductsSituationJob') { SyncProductsSituationJob.perform_now } }
+    log_job('Tiny::Products.assert_stock') { log_motor_activity('Tiny::Products.assert_stock') { Tiny::Products.assert_stock } }
+    log_job('SyncOrdersSituationJob') { log_motor_activity('SyncOrdersSituationJob') { SyncOrdersSituationJob.perform_now } }
   end
 
   private
@@ -17,5 +17,24 @@ class MakeCsvDataJob < ApplicationJob
 
     duration = Time.now - start_time
     Rails.logger.info "=== Fim do #{job_name} (Tempo de execução: #{duration.round(2)} segundos) ==="
+  end
+
+  def log_motor_activity(job_name)
+    motor = Motor.last
+
+    Rails.logger.info "=== Início do motor #{job_name} ==="
+    Rails.logger.info "Start Time: #{motor.start_time}"
+    Rails.logger.info "End Time: #{motor.end_time.nil? ? 'Ainda em progresso' : motor.end_time}"
+    Rails.logger.info "Running Time: #{motor.running_time} segundos"
+    Rails.logger.info "Link: #{motor.link.nil? ? 'Sem link' : motor.link}"
+
+    if motor.end_time.nil?
+      Rails.logger.info "Motor #{job_name} ainda está em execução..."
+    else
+      duration = motor.running_time
+      Rails.logger.info "=== Fim do motor #{job_name} (Tempo de execução: #{duration.round(2)} segundos) ==="
+    end
+
+    yield
   end
 end
