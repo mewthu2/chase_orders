7164ebff7b8f20e05b7fb7588db8271a86b09bba
danielkbr <danielkbr89@gmail.com>
2025-04-21 19:51:39 -0300

    remove helper methods

--- app/models/shopify/orders.rb
+++ app/models/shopify/orders.rb
@@ -1,6 +1,5 @@
 class Shopify::Orders
   require 'shopify_api'
-  extend HelperMethods
   extend ApplicationHelper
 
   class << self
@@ -269,10 +268,10 @@ class Shopify::Orders
         'company' => cliente['nome_fantasia'] || ''
       }
     end
-  
+
     def format_date(date_str)
       return Time.now.iso8601 if date_str.nil? || date_str.empty?
-  
+
       begin
         if date_str.match(%r{\d{2}/\d{2}/\d{4}})
           DateTime.strptime("#{date_str} 12:00:00", '%d/%m/%Y %H:%M:%S').iso8601
@@ -283,49 +282,48 @@ class Shopify::Orders
         Time.now.iso8601
       end
     end
-  
+
     def generate_unique_phone(original_phone, session)
       base_phone = if original_phone.present?
                      format_phone_number(original_phone)
                    else
                      '+5511999999999'
                    end
-  
+
       return base_phone if phone_unique?(base_phone, session)
-  
+
       3.times do |i|
         unique_phone = if base_phone.end_with?('9')
                          base_phone.gsub(/(\d{2})$/, (i + 1).to_s.rjust(2, '0'))
                        else
                          "#{base_phone}#{i + 1}"
                        end
-  
+
         return unique_phone if phone_unique?(unique_phone, session)
       end
-  
+
       "+55119#{SecureRandom.rand(100000000..999999999)}"
     end
-  
+
     def phone_unique?(phone, session)
       return false unless phone.present?
-  
-      # Search for customers with this phone
+
       customers = ShopifyAPI::Customer.all(
         session:,
         phone:,
         limit: 1
       )
-  
+
       customers.empty?
     rescue
       false
     end
-  
+
     def format_phone_number(phone)
       return nil unless phone.present?
-  
+
       cleaned = phone.gsub(/\D/, '')
-  
+
       case cleaned.size
       when 11, 10 then "+55#{cleaned}"
       when 8 then "+5511#{cleaned}"
