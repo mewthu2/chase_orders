30dca95759e74205bca85cfca21317a92c1f8a6a
danielkbr <danielkbr89@gmail.com>
2025-04-22 02:45:05 -0300

    add data to lagoa seca

--- app/jobs/schedule_tiny_to_shopify_job.rb
+++ app/jobs/schedule_tiny_to_shopify_job.rb
@@ -3,7 +3,7 @@ class ScheduleTinyToShopifyJob < ActiveJob::Base
     # Enfileira os pedidos do Tiny para o Shopify
     data = (Date.today - 1).strftime('%d/%m/%Y')
 
-    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'process_for_shopify', '')
+    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'process_for_shopify', data)
     Tiny::Orders.get_all_orders('bh_shopping', 'Entregue', 'process_for_shopify', data)
     Tiny::Orders.get_all_orders('rj', 'Entregue', 'process_for_shopify', data)
   end
