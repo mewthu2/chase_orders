6f4c35c7f6889999e12b9c13f14653941f936667
danielkbr <danielkbr89@gmail.com>
2025-05-05 11:26:39 -0300

    add data to job

--- app/jobs/schedule_tiny_to_shopify_job.rb
+++ app/jobs/schedule_tiny_to_shopify_job.rb
@@ -2,8 +2,8 @@ class ScheduleTinyToShopifyJob < ActiveJob::Base
   def perform
     data = (Date.today - 1).strftime('%d/%m/%Y')
 
-    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'process_for_shopify', '')
-    Tiny::Orders.get_all_orders('bh_shopping', 'Entregue', 'process_for_shopify', '')
-    Tiny::Orders.get_all_orders('rj', 'Entregue', 'process_for_shopify', '')
+    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'process_for_shopify', data)
+    Tiny::Orders.get_all_orders('bh_shopping', 'Entregue', 'process_for_shopify', data)
+    Tiny::Orders.get_all_orders('rj', 'Entregue', 'process_for_shopify', data)
   end
 end
