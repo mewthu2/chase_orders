1b61b3991acf39dcb628899b4c2e0cf79987a6fd
danielkbr <danielkbr89@gmail.com>
2025-05-06 15:44:46 -0300

    fix

--- app/jobs/sync_orders_situation_job.rb
+++ app/jobs/sync_orders_situation_job.rb
@@ -1,19 +1,21 @@
 class SyncOrdersSituationJob < ActiveJob::Base
   def perform
+    data = (Date.today - 1).strftime('%d/%m/%Y')
+
     sync_tiny_orders_lagoa_seca
     sync_tiny_orders_bh_shopping
     sync_tiny_orders_rj
   end
 
   def sync_tiny_orders_lagoa_seca
-    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'update_orders', '')
+    Tiny::Orders.get_all_orders('lagoa_seca', 'Entregue', 'update_orders', data)
   end
 
   def sync_tiny_orders_bh_shopping
-    Tiny::Orders.get_all_orders('bh_shopping', 'Entregue', 'update_orders', '')
+    Tiny::Orders.get_all_orders('bh_shopping', 'Entregue', 'update_orders', data)
   end
 
   def sync_tiny_orders_rj
-    Tiny::Orders.get_all_orders('rj', 'Entregue', 'update_orders', '')
+    Tiny::Orders.get_all_orders('rj', 'Entregue', 'update_orders', data)
   end
 end
